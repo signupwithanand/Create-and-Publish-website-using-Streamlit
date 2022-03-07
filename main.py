9ad56aa6101649bd54c7ed9e4e33969a7d57aa14
signupwithanand <signupwithanand@gmail.com>
2022-03-07 05:53:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Welcome to Streamlit app")
+st.subheader("Introducing Streamlit in Automation")
+st.write('''This is first web app.
+         Enjoy it!
+         ''')
